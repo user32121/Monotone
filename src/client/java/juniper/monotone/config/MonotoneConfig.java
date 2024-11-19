a6461f2d6580618e26c6f3f1bb59a6ebda55e82c
jupiter <pengjupiter5@gmail.com>
2024-11-19 17:10:30 -0600

    proper serialization of RegionMasks

--- src/client/java/juniper/monotone/config/MonotoneConfig.java
+++ src/client/java/juniper/monotone/config/MonotoneConfig.java
@@ -13,6 +13,7 @@ import com.google.gson.GsonBuilder;
 import juniper.monotone.Monotone;
 import juniper.monotone.interaction.InteractionType;
 import juniper.monotone.interaction.RegionMask;
+import juniper.monotone.interaction.RegionMaskAdapter;
 import net.minecraft.client.MinecraftClient;
 
 public class MonotoneConfig {
@@ -24,12 +25,13 @@ public class MonotoneConfig {
     public boolean pathfindShowPath = false;
 
     public Map<InteractionType, Boolean> interactionMaskEnabled = new HashMap<>();
-    //TODO InstanceCreator or TypeAdapter
     public Map<InteractionType, List<RegionMask>> interactionMask = new HashMap<>();
+
+    public static Gson gson = new GsonBuilder().registerTypeAdapter(RegionMask.class, new RegionMaskAdapter()).setPrettyPrinting().create();
 
     public static MonotoneConfig load() {
         try (FileReader fr = new FileReader("config/monotone_config.json")) {
-            MonotoneConfig mc = new Gson().fromJson(fr, MonotoneConfig.class);
+            MonotoneConfig mc = gson.fromJson(fr, MonotoneConfig.class);
             return mc == null ? new MonotoneConfig() : mc;
         } catch (IOException e) {
             Monotone.LOGGER.warn("unable to load config:", e);
@@ -39,7 +41,7 @@ public class MonotoneConfig {
 
     public static void save(MinecraftClient client) {
         try (FileWriter fw = new FileWriter("config/monotone_config.json")) {
-            new GsonBuilder().setPrettyPrinting().create().toJson(Monotone.CONFIG, fw);
+            gson.toJson(Monotone.CONFIG, fw);
         } catch (IOException e) {
             Monotone.LOGGER.warn("unable to save config:", e);
         }
---
+++ src/client/java/juniper/monotone/interaction/RegionMaskAdapter.java
@@ -0,0 +1,50 @@
+package juniper.monotone.interaction;
+
+import java.io.IOException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+public class RegionMaskAdapter extends TypeAdapter<RegionMask> {
+    Gson gson = new Gson();
+
+    @Override
+    public void write(JsonWriter out, RegionMask value) throws IOException {
+        gson.toJson(value);
+        out.beginObject().name("type").value(value.getClass().getName()).name("data").jsonValue(gson.toJson(value)).endObject();
+    }
+
+    @Override
+    public RegionMask read(JsonReader in) throws IOException {
+        if (in.peek() == JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
+        in.beginObject();
+        Class<? extends RegionMask> clazz = null;
+        RegionMask ret = null;
+        while (in.peek() != JsonToken.END_OBJECT) {
+            String name = in.nextName();
+            if (name.equals("type")) {
+                try {
+                    clazz = Class.forName(in.nextString()).asSubclass(RegionMask.class);
+                } catch (ClassNotFoundException | ClassCastException e) {
+                    throw new JsonIOException(e);
+                }
+            } else if (name.equals("data")) {
+                ret = gson.fromJson(in, clazz);
+            } else {
+                in.skipValue();
+            }
+        }
+        if (clazz == null || ret == null) {
+            throw new JsonIOException(String.format("Missing information for constructing RegionMask subtype (type=%s, data=%s)", clazz, ret));
+        }
+        in.endObject();
+        return ret;
+    }
+}
